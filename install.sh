5df3d7adcc35d3dff5d2618f725fa4c34defafd9
hdigiorgi <me@hdigiorgi.com>
2019-02-22 17:51:08 -0300

    add zshrc,omg,lxterminal

--- install.sh
+++ install.sh
@@ -3,7 +3,16 @@
 set -e
 set -x
 
-apt-get install -y i3-wm fish dmenu feh i3status i3blocks \
+sudo apt-get install -y i3-wm fish dmenu feh i3status i3blocks \
         pcmanfm lm-sensors fonts-font-awesome fonts-powerline \
-        i3lock leafpad flameshot dunst lxterminal zsh
+        i3lock leafpad flameshot dunst lxterminal zsh git
 
+rm ~/.config/lxterminal/lxterminal.conf
+rm ~/.zshrc
+
+cp zshrc ~/.zshrc
+cp lxterminal.conf ~/.config/lxterminal/lxterminal.conf
+
+if [ ! -d ~/.oh-my-zsh ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh ~/.zshrc
+fi
---
+++ zshrc
@@ -0,0 +1,13 @@
+
+export ZSH="/home/hdigiorgi/.oh-my-zsh"
+
+ZSH_THEME="robbyrussell"
+
+plugins=(
+  git
+  python
+  pyenv
+)
+
+source $ZSH/oh-my-zsh.sh
+
