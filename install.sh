de3ae5d6e50bd7d6de823956d666ed6c8a356225
hdigiorgi <me@hdigiorgi.com>
2019-02-22 17:35:54 -0300

    locking and lxterminal

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ set -x
 
 apt-get install -y i3-wm fish dmenu feh i3status i3blocks \
         pcmanfm lm-sensors fonts-font-awesome fonts-powerline \
-        i3lock leafpad flameshot dunst
+        i3lock leafpad flameshot dunst lxterminal zsh
 
