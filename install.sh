26908b6dd9b8f3124e5d59341ac515111af3a844
Hernan Ezequiel Di Giorgi <hi@libreneitor.com>
2019-06-30 17:44:00 -0300

    add xautolock dep

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ set -x
 sudo apt-get install -y i3-wm fish dmenu feh i3status i3blocks \
         pcmanfm lm-sensors fonts-font-awesome fonts-powerline \
         i3lock flameshot dunst lxterminal zsh git xcalib \
-        curl fish xfconf dmz-cursor-theme lxappearance
+        curl fish xfconf dmz-cursor-theme lxappearance \
+        xautolock
 
 # lxterminal
 mkdir -p ~/.config/lxterminal/
