d11bdfa2a1b2f4d80371caea0c7979ee624aa909
hdigiorgi <me@hdigiorgi.com>
2019-06-30 14:28:54 -0300

    theme changes

--- install.sh
+++ install.sh
@@ -7,7 +7,18 @@ set -x
 sudo apt-get install -y i3-wm fish dmenu feh i3status i3blocks \
         pcmanfm lm-sensors fonts-font-awesome fonts-powerline \
         i3lock leafpad flameshot dunst lxterminal zsh git xcalib \
-        curl fish
+        curl fish xfconf dmz-cursor-theme lxappearance
 
 cp .zshrc ~/.zshrc
 cp lxterminal.conf ~/.config/lxterminal/lxterminal.conf
+cp xinitrc ~/.xinitrc
+
+# icons, theme
+# lxappearance
+
+# start sudo vim /etc/default/grub -> sudo update-grub
+# GRUB_CMDLINE_LINUX="3"
+#.GRUB_CMDLINE_LINUX_DEFAULT=”text”
+# GRUB_TERMINAL=console
+
+
