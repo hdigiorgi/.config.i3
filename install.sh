ba3b9ab3c0d103bbfe6d3748c4101f0ad2356906
Hernan Ezequiel Di Giorgi <hi@libreneitor.com>
2019-06-30 18:08:52 -0300

    lock when lid is closed

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y i3-wm fish dmenu feh i3status i3blocks \
         pcmanfm lm-sensors fonts-font-awesome fonts-powerline \
         i3lock flameshot dunst lxterminal zsh git xcalib \
         curl fish xfconf dmz-cursor-theme lxappearance \
-        xautolock network-manager
+        xautolock xss-lock network-manager
 
 # lxterminal
 mkdir -p ~/.config/lxterminal/
